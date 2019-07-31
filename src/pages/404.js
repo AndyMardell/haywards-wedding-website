ecea32f813bb23e3df299c403cf830d8d84f3ed8
Andy Mardell <andy@sidigital.co>
2019-07-31 22:39:00 +0100

    Fix location variable

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,7 @@ import Layout from '../layouts'
 import SEO from '../components/seo'
 
 const NotFoundPage = () => (
-  <Layout location={location}>
+  <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
